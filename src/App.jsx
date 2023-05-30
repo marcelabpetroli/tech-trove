b81f5fb44c92f470861b1946c8c5b279a9f4f135
Marcela Petroli <marcelabpetroli@gmail.com>
2023-05-30 15:14:07 +0200

    feat: modify header using styled components and add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import "./styles/App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/header";
 
 function App() {
   return (
     <>
-      <Header />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  background-color: rgb(203, 203, 229);
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const Title = styled.h1`
+  font-size: 1.4rem;
+  color: rgb(42, 42, 42);
+  padding: 0.2rem;
+`;
 
 export const Header = () => {
   return (
-    <section>
-      <h1>TechTrove</h1>
-    </section>
+    <HeaderContainer>
+      <StyledLink to="/">
+        <Title>TechTrove</Title>
+      </StyledLink>
+    </HeaderContainer>
   );
 };
