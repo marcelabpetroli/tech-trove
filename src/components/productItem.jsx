28fb382d557d0cbf3720d7a7b4187b08bffa415c
Marcela Petroli <marcelabpetroli@gmail.com>
2023-05-30 21:16:42 +0200

    style: add styles to items

--- src/components/productItem.jsx
+++ src/components/productItem.jsx
@@ -4,15 +4,31 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Brand = styled.h2`
-  font-size: 2.4rem;
-  color: #242424;
+  font-size: 2.2rem;
+  color: #323232;
   text-transform: uppercase;
   text-align: center;
+  letter-spacing: 0.2rem;
+  background-color: #c5c5c5;
+  border-radius: 3rem;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: #323232;
+    color: #c5c5c5;
+  }
 `;
 
 const Image = styled.img`
   max-width: 100%;
   height: auto;
+  margin-top: 1rem;
+
+  &:hover {
+    border: 0.1rem solid #c5c5c5;
+    border-radius: 10%;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  }
 `;
 
 const Price = styled.h3`
