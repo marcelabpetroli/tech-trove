5501e415982ecd390d060cf32ac41c2c91883214
Marcela Petroli <marcelabpetroli@gmail.com>
2023-05-30 16:30:54 +0200

    refactor: edit header test to use router

--- src/test/header.spec.js
+++ src/test/header.spec.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Header } from "../components/header.jsx";
+import { HashRouter } from "react-router-dom";
 
+const renderWithRouter = () => {
+  render(
+    <HashRouter>
+      <Header />
+    </HashRouter>
+  );
+};
 describe("header spec", () => {
   it("should find the title", () => {
-    render(<Header />);
+    renderWithRouter(<Header />);
 
     const element = screen.getByRole("heading", { name: "TechTrove" });
 
