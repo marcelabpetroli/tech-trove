3863891b9d3836e9e546ed142374a42e4519b0e0
Marcela Petroli <marcelabpetroli@gmail.com>
2023-06-05 12:08:27 +0200

    chore: implement cleaning of cache of ls after one hour

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,13 +1,32 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
   const [value, setValue] = useState(() => {
     const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
+
+    if (storedValue) {
+      const { data } = JSON.parse(storedValue);
+
+      const expirationTime = 1000 * 60 * 60;
+      const now = Date.now();
+      const setupTime = localStorage.getItem("setupTime");
+
+      if (setupTime === null) {
+        localStorage.setItem("setupTime", now);
+      } else if (now - parseInt(setupTime) > expirationTime) {
+        localStorage.removeItem(key);
+        localStorage.setItem("setupTime", now);
+      } else {
+        return data;
+      }
+    }
+
+    return initialValue;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    const dataToStore = { data: value, timestamp: Date.now() };
+    localStorage.setItem(key, JSON.stringify(dataToStore));
   }, [key, value]);
 
   return [value, setValue];
